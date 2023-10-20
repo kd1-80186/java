a50cda73f98e0350cc89fd90757e66ae49de11c0
Anjali-bhise <bhiseav2016@gmail.com>
2023-10-20 19:27:04 +0530

    Assignment 11

---
+++ Assignment11/Assignment11_7/src/com/sunbeam/Labor.java
@@ -0,0 +1,37 @@
+package com.sunbeam;
+
+public class Labor extends Manager
+{
+	private int hours;
+	private double rate;
+	
+	Labor()
+	{
+		
+	}
+
+	public Labor(int hours, double rate) {
+		super();
+		this.hours = hours;
+		this.rate = rate;
+	}
+
+	
+
+	@Override
+	public double getSal() {
+		
+		return this.hours*this.rate;
+	}
+	
+	@Override
+	public double calcIncentives() 
+	{
+		if(this.hours>300)
+		return 0.05*this.getSal();
+		else 
+			return 0.0;
+	}
+
+}
+
---
+++ Assignment11/Assignment11_7/src/com/sunbeam/TotalSalary.java
@@ -0,0 +1,17 @@
+package com.sunbeam;
+
+public class TotalSalary {
+
+	public static void main(String[] args) 
+	{
+		Manager manager = new Manager(1000,200);
+		Labor labor = new Labor(400,100);
+		Clerk clerk = new Clerk(4000);
+		
+		Emp[] e = {manager,labor,clerk};
+		double totalSalary = Emp.calcTotalIncome(e);
+			System.out.print("Total salary of all workers : "+totalSalary);
+		
+	}
+
+}
